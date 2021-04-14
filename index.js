2f8276c92b1ce1f40712f70c7cddc6e9329b2007
Laila Reed <>
2021-04-14 17:17:13 +0000

    finished

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 //Create a variable called myObject and assign 
 //an object to it.
-
+var object = {}
 
 
 
 //Add two properties of firstName and lastName
 //using dot notation
 
-
-
+myObject.firstName = ¨Mister¨
+myOjecy.lastname = ´Corley´
 
 //Add two properties of hobbies and age 
 //using bracket notation
 //hobbies must be an array of at least 4 strings
-
-
+myObject [´age´] = 15
+myObject [´hobbies´] = [´coding´, ´anine´, ´mange´, ´idk´]
 
 
 //Print the myObject variable
-
+//console.log(myobject)
 
 
 
 //Use a for in loop to print all of the keys of the object
-
+for(var key in myObject){
+//console.log(key)
+}
 
 
 
 ///Use a for in loop to print all of the values of the object
+for(var key in myObject){
+  console.log(myObject[key])
+}
 
 
 
@@ -39,3 +44,9 @@ create an object that is about a specific car
 Include: year, make, model, color and an array of features that you want your car to have
 
 */
+var car = {
+  year: 2017,
+  make: ´Charger´
+  model: srt
+  features:[ radio, pone gps, windows]
+}
